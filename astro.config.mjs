af463c98d512d1f06fe5e613af5447f9ccd453da
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2025-09-28 10:00:03 -0700

    Clean it up, slightly

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,16 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Obelisk',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withobelisk' }],
 			sidebar: [
 				{
 					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					autogenerate: { directory: 'guides' },
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Products',
+					autogenerate: { directory: 'products' },
 				},
 			],
 		}),
