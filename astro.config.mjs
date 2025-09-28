f6cfdef3bd3b74f146784c508a5b77ee961544d7
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2025-09-28 10:03:14 -0700

    fix github link

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Obelisk',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withobelisk' }],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/obeliskhq' }],
 			sidebar: [
 				{
 					label: 'Guides',
